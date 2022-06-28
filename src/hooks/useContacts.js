ebd766b76551a0c66d0851ce894b53e775049ad9
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-06-28 21:53:01 +0300

    a

--- src/hooks/useContacts.js
+++ src/hooks/useContacts.js
@@ -26,35 +26,11 @@ const [deleteContact] = useDeleteContactMutation();
   };
 }
 
-// export function useContacts() {
-//   const contacts = useSelector(state => state.contacts.items);
 //   const filter = useSelector(state => state.contacts.filter);
 
-//   const dispatch = useDispatch();
-
-//   const addContact = newContact => dispatch(addContactReducer(newContact));
-//   const deleteContact = id => dispatch(deleteContactReducer(id));
 //   const filterValue = value =>
 //     dispatch(setFilterReducer(transformValue(value)));
-
-//   const transformValue = value => value.toLowerCase().trim();
-
-//   const isContactExists = newContact =>
-//     contacts.find(
-//       contact =>
-//         transformValue(contact.name) === transformValue(newContact.name),
-//     );
 
 //   const filterContacts = () =>
 //     contacts.filter(contact => contact.name.includes(filter));
 
-//   return {
-//     contacts,
-//     filter,
-//     deleteContact,
-//     addContact,
-//     isContactExists,
-//     filterValue,
-//     filterContacts,
-//   };
-// }
